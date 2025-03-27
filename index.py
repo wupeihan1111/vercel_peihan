ebc6c3afdcb18549d7c580c6a7bdd93890fefec8
wupeihan1111 <wupeihan.1111@gmail.com>
2025-03-27 10:02:09 +0800

    Add files via upload

--- index.py
+++ index.py
@@ -7,11 +7,10 @@ def index():
     homepage = "<h1>吳沛涵Python網頁(時間+8)</h1>"
     homepage += "<a href=/mis>MIS</a><br>"
     homepage += "<a href=/today>顯示日期時間</a><br>"
-    homepage += "<a href=/welcome?nick=peihan>傳送使用者暱稱</a><br>"
+    homepage += "<a href=/welcome?nick=peihan&work=pu>傳送使用者暱稱</a><br>"
     homepage += "<a href=/account>網頁表單傳值</a><br>"
     homepage += "<a href=/rwd>沛涵簡介網頁</a><br>"
     return homepage
-
 
 
 
@@ -32,7 +31,8 @@ def me():
 @app.route("/welcome", methods=["GET"])
 def welcome():
     user = request.values.get("nick")
-    return render_template("welcome.html", name=user)
+    w = request.values.get("work")
+    return render_template("welcome.html", name=user,work=w)
 
 @app.route("/account", methods=["GET", "POST"])
 def account():
